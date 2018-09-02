c773a41234bcf4e2e6cb97ec620cbdd7225be242
Hugo Tonette <hugo.tonette@gmail.com>
2018-09-02 09:06:26 -0300

    .bjbbj

---
+++ TeamPenys/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    AudioSource myAudio;
+    public GameObject objecto;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!myAudio.isPlaying)
+        {
+            objecto.transform.gameObject.SetActive(true);
+        }
+	}
+}
